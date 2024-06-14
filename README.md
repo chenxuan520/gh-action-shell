ff615620bb5a09c597b33caaea222e2083c9a698
chenxuan <1607772321@qq.com>
2024-06-14 13:32:46 +0800

    feat:add first shell

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gh-action-shell
-Some useful shell scripts that can be used directly github-action
+- 一些有用的shell脚本,可用于github-action,因为懒得编写一个新的action
+# 使用方法
+## git_commit_message_from_last_tag.sh
+- 运行后输出为当前提交到上一个tag生成的所有commit message信息,用于release body的自动化生成
---
+++ git_commit_message_from_last_tag.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 确保我们在一个git仓库中
+if ! [ -d ".git" ]; then
+    echo "当前目录不是一个git仓库"
+    exit 1
+fi
+
+# 尝试获取上一个tag的commit hash（或者就是当前HEAD，如果它是一个tag）
+last_tag_commit=$(git describe --tags --abbrev=0 HEAD 2>/dev/null)
+
+# 获取commit message
+if [ -n "$last_tag_commit" ]; then
+    # 如果有上一个tag（或者就是当前HEAD），则获取从上一个tag（包括这个tag）到其之前的所有commit的hash
+    commits=$(git log --pretty=%s "$last_tag_commit"..HEAD)
+else
+    # 如果没有上一个tag，则获取从开始到HEAD的所有commit的hash
+    commits=$(git log --pretty=%s)
+fi
+
+# 打印每个commit message
+echo "$commits"
